3905328555a33cb7715695fe36c5127764a3afa5
Cydo Entis <cydoentis@gmail.com>
2025-12-12 23:11:00 -0500

    feat: added InternalServerError error code to ErrorCodes constants

--- ErrorHound/Core/ErrorCodes.cs
+++ ErrorHound/Core/ErrorCodes.cs
@@ -5,4 +5,5 @@ public static class ErrorCodes
     public const string ValidationError = "VALIDATION_ERROR";
     public const string NotFoundError = "NOT_FOUND";
     public const string UnauthorizedError = "UNAUTHORIZED";
+    public const string InternalServerError = "INTERNAL_SERVER_ERROR";
 }
