dee726d5ed7c876cf86f6d41b58c718162ab3c85
Cydo Entis <cydoentis@gmail.com>
2025-12-13 13:47:53 -0500

    feat: added matching error code cosntants for all newly added built in errors

--- ErrorHound/Core/ErrorCodes.cs
+++ ErrorHound/Core/ErrorCodes.cs
@@ -2,8 +2,15 @@
 
 public static class ErrorCodes
 {
-    public const string ValidationError = "VALIDATION_ERROR";
-    public const string NotFoundError = "NOT_FOUND";
-    public const string UnauthorizedError = "UNAUTHORIZED";
-    public const string InternalServerError = "INTERNAL_SERVER_ERROR";
+    public const string Validation = "VALIDATION";
+    public const string NotFound = "NOT_FOUND";
+    public const string Unauthorized = "UNAUTHORIZED";
+    public const string InternalServer = "INTERNAL_SERVER";
+    public const string Forbidden = "FORBIDDEN";
+    public const string Conflict = "CONFLICT";
+    public const string Database = "DATABASE";
+    public const string BadRequest = "BAD_REQUEST";
+    public const string TooManyRequests = "TOO_MANY_REQUESTS";
+    public const string ServiceUnavailable = "SERVICE_UNAVAILABLE";
+    public const string Timeout = "TIMEOUT";
 }
