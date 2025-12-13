5352f172dc20f51bdba3f2b360ddb9fc0b498640
Cydo Entis <cydoentis@gmail.com>
2025-12-12 23:11:34 -0500

    feat: added an ErrorHoundOptions.cs that will take a Func that allows users to use their own ApiResponses

---
+++ ErrorHound/Middleware/ErrorHoundOptions.cs
@@ -0,0 +1,12 @@
+﻿using ErrorHound.Core;
+
+namespace ErrorHound.Middleware;
+
+public class ErrorHoundOptions
+{
+    /// <summary>
+    /// Optional delegate to wrap ApiError objects into a custom response object.
+    /// If null, the default response structure is used.
+    /// </summary>
+    public Func<ApiError, object>? ResponseWrapper { get; set; }
+}
