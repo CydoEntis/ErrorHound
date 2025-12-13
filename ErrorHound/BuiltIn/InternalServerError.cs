bf33420ae13141525b292ad15ae8d7bee9006056
Cydo Entis <cydoentis@gmail.com>
2025-12-12 23:11:47 -0500

    feat: added a generic InternalServerError.cs

---
+++ ErrorHound/BuiltIn/InternalServerError.cs
@@ -0,0 +1,20 @@
+﻿namespace ErrorHound.BuiltIn;
+
+using System.Net;
+using ErrorHound.Core;
+
+/// <summary>
+/// Represents a generic internal server error.
+/// </summary>
+public class InternalServerError : ApiError
+{
+    public InternalServerError(string? details = null)
+        : base(
+            code: ErrorCodes.InternalServerError,
+            message: "An unexpected error occurred",
+            status: (int)HttpStatusCode.InternalServerError,
+            details: details
+        )
+    {
+    }
+}
